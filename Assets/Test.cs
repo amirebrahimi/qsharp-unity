a1d9c681c97d3e45d7e8d044a094b4d967841661
Amir Ebrahimi <amir@unity3d.com>
2021-05-25 16:52:22 -0700

    Check in Unity parts (includes pre-compiled Q# host)

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Quantum.Diagnostics;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using UnityEngine;
+using quantum;
+
+static class IEnumerableExtensions
+{
+    public static bool Parity(this IEnumerable<bool> bitVector) =>
+        bitVector.Aggregate(
+            (acc, next) => acc ^ next
+        );
+
+    public static string ToDelimitedString(this IEnumerable<bool> values) =>
+        String.Join(", ", values.Select(x => x.ToString()));
+}
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        UnitySystemConsoleRedirector.Redirect();
+
+        var simulator = new QuantumSimulator();
+        var bits = new[] { false, true, false };
+        using (var sim = new QuantumSimulator())
+        {
+            Console.WriteLine($"Input: {bits.ToDelimitedString()}");
+
+            var restored = await RunAlgorithm.Run(sim, new QArray<bool>(bits));
+            Console.WriteLine($"Output: {restored.ToDelimitedString()}");
+
+            Debug.Assert(bits.Parity() == restored.Parity());
+        }
+    }
+}
